782c02b910728544a49208cd984f20fa6a2f6204
Karalos <carloseas1@al.insper.edu.br>
2020-06-14 19:33:27 -0300

    mais organizacao

--- TELA PRINCIPAL.py
+++ TELA PRINCIPAL.py
@@ -16,20 +16,19 @@ pasta_img=path.join(path.dirname(__file__), 'imagens')
 instrucao = 0
 jogo = 1
 fim = 2
-sair=3
 
 #variaveis
     #tela
 altura=600
 largura=1000
-    #cores
-cor=(0,0,255)
-cor1=(0,200,0)
-cors=(0,0,0)
-core=(127,127,127)
-cormg=(230,170,0)
+    #azules
+azul=(0,0,255)
+verde=(0,200,0)
+preto=(0,0,0)
+azule=(127,127,127)
+vermelho=(230,170,0)
     #posicao inicial
-poscir=[100,100]
+posi_joga=[100,100]
 
 #inicia jogo
 pygame.init()
@@ -90,7 +89,7 @@ verts1819=[(352,453),(354,252+201),(354,353+201),(352,353+201)]
 verts1920=[(453,453),(454,453),(454,555),(453,555)]
 verts20fim=[(554,453),(556,453),(556,555),(554,555)]
 
-#textos e posicoes das casas
+#textos das casas
     #casa inicial
 textini=font.render('Início',True,(0,0,0))
 xini=80 
@@ -104,15 +103,15 @@ y1=90
     #casa 2
 text2=font.render('2',True,(0,0,0))
 x2=252+42
-
+y2=y1
     #casa 3
 text3=font.render('3',True,(0,0,0))
 x3=353+42
-
+y3=y1
     #casa 4
 text4=font.render('4',True,(0,0,0))
 x4=454+42
-
+y4=y1
     #casa 5 
 text5=fontt.render('VOLTE',True,(255,0,0))
 x5=580
@@ -124,11 +123,12 @@ y5s=105
     #casa 6
 text6=font.render('6',True,(0,0,0))
 x6=656+42
+y6=y1
 
     #casa 7
 text7=font.render('7',True,(0,0,0))
 x7=757+42
-
+y7=y1
     #casa 8 
 text8=fontt.render('AVANCE',True,(255,0,0))
 x8=775
@@ -145,19 +145,19 @@ y9=251+40
     #casa 10
 text10=font.render('10',True,(0,0,0))
 x10=656+37
-
+y10=y9
     #casa 11
 text11=font.render('11',True,(0,0,0))
 x11=555+37
-
+y11=y9
     #casa 12
 text12=font.render('12',True,(0,0,0))
 x12=454+37
-
+y12=y9
     #casa 13
 text13=font.render('13',True,(0,0,0))
 x13=353+37
-
+y13=y9
     #casa 14
 text14=font.render('14',True,(0,0,0))
 x14=252+37
@@ -184,17 +184,17 @@ y17=453+37
     #casa 18
 text18=font.render('18',True,(0,0,0))
 x18=252+37
-y18=453+37
+y18=y17
 
     #casa 19
 text19=font.render('19',True,(0,0,0))
 x19=353+37
-y19=453+37
+y19=y17
 
     #casa 20
 text20=fontt.render('VOLTE PARA',True,(255,0,0))
 x20=420+37
-y20=453+37
+y20=y17
 text20s=fontt.render('O INICIO',True,(255,0,0))
 x20s=440+37
 y20s=473+37
@@ -202,7 +202,7 @@ y20s=473+37
     #casa final
 textfim=font.render('Fim',True,(0,0,0))
 xfim=555+18
-yfim=453+39
+yfim=y17
 
 #numero resultado do dado
 textdado1=fontdado.render('DADO:',True,(255,0,0))
@@ -213,31 +213,6 @@ FPS = 60
 
 #funcoes
 def tela_de_instrucoes(tela):
-    clock = pygame.time.Clock()
-    #imagem
-    background = pygame.image.load(path.join(pasta_img, 'imagem1.jpg')).convert()
-    background_rect = background.get_rect()
-    #codigo jogo
-    jogando_i = True
-    while jogando_i:
-        clock.tick(FPS)
-        #eventos
-        for event in pygame.event.get():
-            #fechar o jogo
-            if event.type == pygame.QUIT:
-                condicao = fim
-                jogando_i = False
-            #ir para proxima fase
-            if event.type == pygame.KEYDOWN:
-                condicao = jogo
-                jogando_i = False
-
-        tela.fill((0,0,0))
-        tela.blit(background, background_rect)
-        pygame.display.flip()
-    return condicao
-
-def tela_fim(tela):
     clock = pygame.time.Clock()
     #imagem
     background = pygame.image.load(path.join(pasta_img, 'imagem1.jpg')).convert()
@@ -278,229 +253,232 @@ def tela_jogo(tela):
                     DADO=dado
                     contador+=dado
                     if contador==1:
-                        poscir[0]=2*101
-                        poscir[1]=100 
+                        posi_joga[0]=2*101
+                        posi_joga[1]=100 
                         CASA1(window)  
                     if contador==2:
-                        poscir[0]=3*101
-                        poscir[1]=100
+                        posi_joga[0]=3*101
+                        posi_joga[1]=100
                         CASA2(window) 
                     if contador==3:
-                        poscir[0]=4*101
-                        poscir[1]=100
+                        posi_joga[0]=4*101
+                        posi_joga[1]=100
                     if contador==4:
-                        poscir[0]=5*101
-                        poscir[1]=100
+                        posi_joga[0]=5*101
+                        posi_joga[1]=100
                         CASA3(window)
                     if contador==5:
-                        poscir[0]=4*101
-                        poscir[1]=100
+                        posi_joga[0]=4*101
+                        posi_joga[1]=100
                         contador-=2 
                     if contador==6:
-                        poscir[0]=7*101
-                        poscir[1]=100
+                        posi_joga[0]=7*101
+                        posi_joga[1]=100
                     if contador==7:
-                        poscir[0]=808
-                        poscir[1]=100
+                        posi_joga[0]=808
+                        posi_joga[1]=100
                         CASA4(window)
                     if contador==8:
-                        poscir[1]=100
-                        poscir[0]=606
+                        posi_joga[1]=100
+                        posi_joga[0]=606
                         contador+=3
                     if contador==9:
-                        poscir[0]=808
-                        poscir[1]=302
+                        posi_joga[0]=808
+                        posi_joga[1]=302
                     if contador==10:
-                        poscir[0]=707
-                        poscir[1]=302
+                        posi_joga[0]=707
+                        posi_joga[1]=302
                         CASA5(window)
                     if contador==11:
-                        poscir[0]=606
-                        poscir[1]=302
+                        posi_joga[0]=606
+                        posi_joga[1]=302
                     if contador==12:
-                        poscir[0]=505
-                        poscir[1]=302
+                        posi_joga[0]=505
+                        posi_joga[1]=302
                         CASA6(window)
                     if contador==13:
-                        poscir[0]=404
-                        poscir[1]=302
+                        posi_joga[0]=404
+                        posi_joga[1]=302
                         CASA7(window)
                     if contador==14:
-                        poscir[0]=303
-                        poscir[1]=302
+                        posi_joga[0]=303
+                        posi_joga[1]=302
                         CASA8(window)
                     if contador==15:
-                        poscir[0]=202 
-                        poscir[1]=504 
+                        posi_joga[0]=202 
+                        posi_joga[1]=504 
                         CASA2(window)
                     if contador==16:
-                        poscir[0]=202
-                        poscir[1]=403
+                        posi_joga[0]=202
+                        posi_joga[1]=403
                         CASA6(window)
                     if contador==17:
-                        poscir[0]=202
-                        poscir[1]=504
+                        posi_joga[0]=202
+                        posi_joga[1]=504
                     if contador==18:
-                        poscir[0]=303
-                        poscir[1]=504
+                        posi_joga[0]=303
+                        posi_joga[1]=504
                         CASA3(window)
                     if contador==19:
-                        poscir[0]=404
-                        poscir[1]=504
+                        posi_joga[0]=404
+                        posi_joga[1]=504
                     if contador==20:
-                        poscir[0]=100 
-                        poscir[1]=100 
+                        posi_joga[0]=100 
+                        posi_joga[1]=100 
                         contador-=20
                     if contador>21:
                         contador=21
                     if contador==21:
-                        poscir[0]=591
-                        poscir[1]=504 
+                        posi_joga[0]=591
+                        posi_joga[1]=504 
+
                 if event.key == pygame.K_SPACE and flow == 'repita':
                     if contador==1:
-                        poscir[0]=2*101
-                        poscir[1]=100 
+                        posi_joga[0]=2*101
+                        posi_joga[1]=100 
                         CASA1(window)  
                     if contador==2:
-                        poscir[0]=3*101
-                        poscir[1]=100
+                        posi_joga[0]=3*101
+                        posi_joga[1]=100
                         CASA2(window) 
                     if contador==3:
-                        poscir[0]=4*101
-                        poscir[1]=100
+                        posi_joga[0]=4*101
+                        posi_joga[1]=100
                     if contador==4:
-                        poscir[0]=5*101
-                        poscir[1]=100
+                        posi_joga[0]=5*101
+                        posi_joga[1]=100
                         CASA3(window)
                     if contador==5:
-                        poscir[0]=4*101
-                        poscir[1]=100
+                        posi_joga[0]=4*101
+                        posi_joga[1]=100
                         contador-=2 
                     if contador==6:
-                        poscir[0]=7*101
-                        poscir[1]=100
+                        posi_joga[0]=7*101
+                        posi_joga[1]=100
                     if contador==7:
-                        poscir[0]=808
-                        poscir[1]=100
+                        posi_joga[0]=808
+                        posi_joga[1]=100
                         CASA4(window)
                     if contador==8:
-                        poscir[1]=100
-                        poscir[0]=606
+                        posi_joga[1]=100
+                        posi_joga[0]=606
                         contador+=3
                     if contador==9:
-                        poscir[0]=808
-                        poscir[1]=302
+                        posi_joga[0]=808
+                        posi_joga[1]=302
                     if contador==10:
-                        poscir[0]=707
-                        poscir[1]=302
+                        posi_joga[0]=707
+                        posi_joga[1]=302
                         CASA5(window)
                     if contador==11:
-                        poscir[0]=606
-                        poscir[1]=302
+                        posi_joga[0]=606
+                        posi_joga[1]=302
                     if contador==12:
-                        poscir[0]=505
-                        poscir[1]=302
+                        posi_joga[0]=505
+                        posi_joga[1]=302
                         CASA6(window)
                     if contador==13:
-                        poscir[0]=404
-                        poscir[1]=302
+                        posi_joga[0]=404
+                        posi_joga[1]=302
                         CASA7(window)
                     if contador==14:
-                        poscir[0]=303
-                        poscir[1]=302
+                        posi_joga[0]=303
+                        posi_joga[1]=302
                         CASA8(window)
                     if contador==15:
-                        poscir[0]=202 
-                        poscir[1]=504 
+                        posi_joga[0]=202 
+                        posi_joga[1]=504 
                         CASA2(window)
                     if contador==16:
-                        poscir[0]=202
-                        poscir[1]=403
+                        posi_joga[0]=202
+                        posi_joga[1]=403
                         CASA6(window)
                     if contador==17:
-                        poscir[0]=202
-                        poscir[1]=504
+                        posi_joga[0]=202
+                        posi_joga[1]=504
                     if contador==18:
-                        poscir[0]=303
-                        poscir[1]=504
+                        posi_joga[0]=303
+                        posi_joga[1]=504
                         CASA3(window)
                     if contador==19:
-                        poscir[0]=404
-                        poscir[1]=504
+                        posi_joga[0]=404
+                        posi_joga[1]=504
                     if contador==20:
-                        poscir[0]=100 
-                        poscir[1]=100 
+                        posi_joga[0]=100 
+                        posi_joga[1]=100 
                         contador-=20
                     if contador>21:
                         contador=21
                     if contador==21:
-                        poscir[0]=591
-                        poscir[1]=504 
+                        posi_joga[0]=591
+                        posi_joga[1]=504 
 
         window.blit(background, (0, 0))
-        ####################################################
-        pygame.draw.polygon(window, cor1, vertfim)
-        pygame.draw.polygon(window, cor, vert20)
-        pygame.draw.polygon(window, cormg, vert19)
-        pygame.draw.polygon(window, cor, vert18)
-        pygame.draw.polygon(window, cormg, vert17)
-        pygame.draw.polygon(window, cor, vert16)
-        pygame.draw.polygon(window, cor, vert15)
-        pygame.draw.polygon(window, cor, vert14)
-        pygame.draw.polygon(window, cor, vert13)
-        pygame.draw.polygon(window, cor, vert12)
-        pygame.draw.polygon(window, cormg, vert11)
-        pygame.draw.polygon(window, cor, vert10)
-        pygame.draw.polygon(window, cormg, vert9)       
-        pygame.draw.polygon(window, cor, vert8)
-        pygame.draw.polygon(window, cor, vert7)
-        pygame.draw.polygon(window, cormg, vert6)
-        pygame.draw.polygon(window, cor, vert5)
-        pygame.draw.polygon(window, cor, vert4)
-        pygame.draw.polygon(window, cormg, vert3)
-        pygame.draw.polygon(window, cor, vert2)
-        pygame.draw.polygon(window, cor, vert1)
-        ######################################################
-        pygame.draw.polygon(window, cor1, vertini)
-        pygame.draw.polygon(window, cors, vertsi1)
-        pygame.draw.polygon(window, cors, verts12)
-        pygame.draw.polygon(window, cors, verts23)
-        pygame.draw.polygon(window, cors, verts34)
-        pygame.draw.polygon(window, cors, verts45)
-        pygame.draw.polygon(window, cors, verts56)
-        pygame.draw.polygon(window, cors, verts67)
-        pygame.draw.polygon(window, cors, verts78)
-        pygame.draw.polygon(window, cors, verts89)
-        pygame.draw.polygon(window, cors, verts910)
-        pygame.draw.polygon(window, cors, verts1011)
-        pygame.draw.polygon(window, cors, verts1112)
-        pygame.draw.polygon(window, cors, verts1213)
-        pygame.draw.polygon(window, cors, verts1314)
-        pygame.draw.polygon(window, cors, verts1415)
-        pygame.draw.polygon(window, cors, verts1516)
-        pygame.draw.polygon(window, cors, verts1617)
-        pygame.draw.polygon(window, cors, verts1718)
-        pygame.draw.polygon(window, cors, verts1819)
-        pygame.draw.polygon(window, cors, verts1920)
-        pygame.draw.polygon(window, cors, verts20fim)
+
+        #desenha casas
+        pygame.draw.polygon(window, verde, vertfim)
+        pygame.draw.polygon(window, azul, vert20)
+        pygame.draw.polygon(window, vermelho, vert19)
+        pygame.draw.polygon(window, azul, vert18)
+        pygame.draw.polygon(window, vermelho, vert17)
+        pygame.draw.polygon(window, azul, vert16)
+        pygame.draw.polygon(window, azul, vert15)
+        pygame.draw.polygon(window, azul, vert14)
+        pygame.draw.polygon(window, azul, vert13)
+        pygame.draw.polygon(window, azul, vert12)
+        pygame.draw.polygon(window, vermelho, vert11)
+        pygame.draw.polygon(window, azul, vert10)
+        pygame.draw.polygon(window, vermelho, vert9)       
+        pygame.draw.polygon(window, azul, vert8)
+        pygame.draw.polygon(window, azul, vert7)
+        pygame.draw.polygon(window, vermelho, vert6)
+        pygame.draw.polygon(window, azul, vert5)
+        pygame.draw.polygon(window, azul, vert4)
+        pygame.draw.polygon(window, vermelho, vert3)
+        pygame.draw.polygon(window, azul, vert2)
+        pygame.draw.polygon(window, azul, vert1)
+        pygame.draw.polygon(window, verde, vertini)
+
+        #desenha linhas
+        pygame.draw.polygon(window, preto, vertsi1)
+        pygame.draw.polygon(window, preto, verts12)
+        pygame.draw.polygon(window, preto, verts23)
+        pygame.draw.polygon(window, preto, verts34)
+        pygame.draw.polygon(window, preto, verts45)
+        pygame.draw.polygon(window, preto, verts56)
+        pygame.draw.polygon(window, preto, verts67)
+        pygame.draw.polygon(window, preto, verts78)
+        pygame.draw.polygon(window, preto, verts89)
+        pygame.draw.polygon(window, preto, verts910)
+        pygame.draw.polygon(window, preto, verts1011)
+        pygame.draw.polygon(window, preto, verts1112)
+        pygame.draw.polygon(window, preto, verts1213)
+        pygame.draw.polygon(window, preto, verts1314)
+        pygame.draw.polygon(window, preto, verts1415)
+        pygame.draw.polygon(window, preto, verts1516)
+        pygame.draw.polygon(window, preto, verts1617)
+        pygame.draw.polygon(window, preto, verts1718)
+        pygame.draw.polygon(window, preto, verts1819)
+        pygame.draw.polygon(window, preto, verts1920)
+        pygame.draw.polygon(window, preto, verts20fim)
         
-        #############################################################
+        #desenha textos
         window.blit(textini, (xini,yini))
         window.blit(text1, (x1,y1))
-        window.blit(text2, (x2,y1))
-        window.blit(text3, (x3,y1))
-        window.blit(text4, (x4,y1))
+        window.blit(text2, (x2,y2))
+        window.blit(text3, (x3,y3))
+        window.blit(text4, (x4,y4))
         window.blit(text5, (x5,y5))
         window.blit(text5s, (x5s,y5s))
-        window.blit(text6, (x6,y1))
-        window.blit(text7, (x7,y1))
+        window.blit(text6, (x6,y6))
+        window.blit(text7, (x7,y7))
         window.blit(text8, (x8,y8))
         window.blit(text8s, (x8s,y8s))
         window.blit(text9, (x9,y9))
-        window.blit(text10, (x10,y9))
-        window.blit(text11, (x11,y9))
-        window.blit(text12, (x12,y9))
-        window.blit(text13, (x13,y9))
+        window.blit(text10, (x10,y10))
+        window.blit(text11, (x11,y11))
+        window.blit(text12, (x12,y12))
+        window.blit(text13, (x13,y13))
         window.blit(text14, (x14,y14))
         window.blit(text15, (x15,y15))
         window.blit(text15s, (x15s,y15s))
@@ -513,11 +491,14 @@ def tela_jogo(tela):
         window.blit(textfim, (xfim,yfim))
         window.blit(textdado1, (700,500))
         window.blit(fontdado.render(str(DADO),True,(255,0,0)), (920,500))
-        #############################################################
-        pygame.draw.circle(window,core,poscir,25)
+
+        #jogadores
+        #desenha peao
+        pygame.draw.circle(window,azule,posi_joga,25)
+        #bot1
+        #bot2
+        #update
         pygame.display.update() 
-
-
 
 #roda o jogo
 condicao = instrucao
@@ -526,9 +507,7 @@ while condicao != fim:
         condicao = tela_de_instrucoes(window)
     elif condicao == jogo:
         condicao = tela_jogo(window)
-    elif condicao==fim:
-        condicao = tela_fim(window)
     else:
-        condicao = sair
+        condicao = fim
 #encerra o jogo
 pygame.quit()  
